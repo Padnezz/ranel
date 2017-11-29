35cbbe195150c1e8cef068a7de0d555ce6a9632e
André Pettersson <padnezz@gmail.com>
2017-11-29 02:12:51 +0100

    FUUUUUUUUUUUUUUUU

--- src/main.php
+++ src/main.php
@@ -5,18 +5,6 @@
  * @Date: 2017-11-23 20:57:43 
   */
 
-namespace ranel;
 
-include_once(dirname(__FILE__)."/include/classes/Database.php");
-
-try
-{
-    $MySQL = new \ranel\Database\Database();
-}
-catch(\Exception $e)
-{
-    echo "Error caught " . $e;
-    exit(1);
-}
 
 ?>
