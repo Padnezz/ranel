4710b06626c8748e2a9cc6f159c56e38e8605dbf
André Pettersson <padnezz@gmail.com>
2017-12-02 01:36:06 +0100

    Fine tuning and added login page

--- src/include/config.php
+++ src/include/config.php
@@ -9,30 +9,35 @@ namespace ranel\Config;
 
 
 class General{
-    //The company name that will be shown on the website
+    //string. The company name that will be shown on the website
     const COMPANYNAME = 'Brand Inc.';    
 
-    //If errors should be transfered in plain text to the user on the website.
+    //boolean. If errors should be transfered in plain text to the user on the website.
     const DEBUG = true;
 
-    //Should non-php errors exit the script?
+    //boolean. Should non-php errors exit the script?
     const DEBUGEXIT = true;
 }
 
 class Design{
-    //Set primary color and secondary color and use it according to Material Design
+    //string. Set primary color and secondary color and use it according to Material Design
     const PRIMARYCOLOR = "#2196F3";
+    //string
     const SECONDARYCOLOR = "#9C27B0";
 
-    //What theme should be used on the site? Possible arguments is DARK or LIGHT
+    //string. What theme should be used on the site? Possible arguments is DARK or LIGHT
     const THEME = "LIGHT";
 }
 
 //Database configuration for MySQL
 class MySQL{
+    //string. The name of the database we should use
     const DATABASE = "ranel";
+    //string. IP or FQDN to the database
     const HOST = "127.0.0.1";
+    //string. Username to the database
     const USERNAME = "user";
+    //string. Password to the database
     const PASSWORD = "pass";
 }
 
--- src/index.php
+++ src/index.php
@@ -8,5 +8,71 @@
 namespace ranel;
 
 include_once(dirname(__FILE__)."/include/config.php");
+include_once(dirname(__FILE__)."/include/general_init.php");
 
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title><?php echo \ranel\Config\General::COMPANYNAME; ?> - Inloggning</title>
+
+    <link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+
+    <script>
+      $(document).ready(
+        function(){
+          $("body").css("background-color", "<?php echo $theme_body_color; ?>");
+          $(".index-nav").css("background-color", "<?php echo \ranel\Config\Design::PRIMARYCOLOR; ?>");
+          $(".signin-btn").css("background-color", "<?php echo \ranel\Config\Design::SECONDARYCOLOR; ?>");
+          colorText(".signin-btn");
+          colorText(".index-nav");
+          if($("body").css("background-color") == "rgb(250, 250, 250)")
+          {
+            $("label").css("color", "black");
+            $("h2").css("color", "black");
+          }
+          if($("body").css("background-color") == "rgb(48, 48, 48)")
+          {
+            $("label").css("color", "white");
+            $("h2").css("color", "white");
+          }
+        }
+      )
+    </script>
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light index-nav"><h3><?php echo \ranel\Config\General::COMPANYNAME; ?></h3></nav>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-block signin-btn" type="submit">Sign in</button>
+      </form>
+
+    </div>
+  </body>
+</html>
