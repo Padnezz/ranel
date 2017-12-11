f449d5c7864d1df09214dcd65c5e5fc6e00030d8
André Pettersson <padnezz@gmail.com>
2017-12-11 01:48:28 +0100

    im sad

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+php_error="false"
 for f in $(find ./src -name '*.php')
 do
 	php -f $f > /dev/null
+	if [ $? -eq 255 ]; then
+		php_error="true"
+	fi
 done
+if [ "$php_error" = "true" ]; then
+	exit 255
+fi
