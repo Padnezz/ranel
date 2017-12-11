398c7314039defd923f7047d3fcf5c529cb4964b
André Pettersson <padnezz@gmail.com>
2017-12-11 01:57:25 +0100

    sraaasstatatata

--- src/include/classes/DebugError.php
+++ src/include/classes/DebugError.php
@@ -11,7 +11,7 @@ namespace ranel;
 require_once(dirname(__FILE__)."/../config.php");
 
 class DebugError{
-    public static function shouldBeHandledByClass()
+    public function shouldBeHandledByClass()
     {
         if(\ranel\Config\General::DEBUG || \ranel\Config\General::DEBUGEXIT)
         {
@@ -24,6 +24,7 @@ class DebugError{
     }
 
     public static function error($error)
+    public function error($error)
     {
         if(\ranel\Config\General::DEBUG)
         {
