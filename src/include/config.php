1b174ede60ddd27100cb854a2de7bf73f5b1b319
Padnezz <padnezz@gmail.com>
2017-12-12 21:10:34 +0000

    Apply fixes from StyleCI

--- src/include/config.php
+++ src/include/config.php
@@ -1,17 +1,17 @@
 <?php
 
 /*
- * @Author: André Pettersson 
- * @Date: 2017-11-23 20:56:34 
+ * @Author: André Pettersson
+ * @Date: 2017-11-23 20:56:34
  * Contains all the configuration
 */
 
 namespace ranel\Config;
 
-
-class General{
+class General
+{
     //string. The company name that will be shown on the website
-    const COMPANYNAME = 'Brand Inc.';    
+    const COMPANYNAME = 'Brand Inc.';
 
     //boolean. If errors should be transfered in plain text to the user on the website.
     const DEBUG = true;
@@ -20,31 +20,32 @@ class General{
     const DEBUGEXIT = true;
 }
 
-class Design{
+class Design
+{
     //string. Set primary color and secondary color and use it according to Material Design
-    const PRIMARYCOLOR = "#2196F3";
+    const PRIMARYCOLOR = '#2196F3';
     //string
-    const SECONDARYCOLOR = "#9C27B0";
+    const SECONDARYCOLOR = '#9C27B0';
 
     //string. What theme should be used on the site? Possible arguments is DARK or LIGHT
-    const THEME = "LIGHT";
+    const THEME = 'LIGHT';
 }
 
-class Database{
-    //string. The database engine to use    
-    const ENGINE = "MySQL";
+class Database
+{
+    //string. The database engine to use
+    const ENGINE = 'MySQL';
 }
 
 //Database configuration for MySQL
-class MySQL{
+class MySQL
+{
     //string. The name of the database we should use
-    const DATABASE = "ranel";
+    const DATABASE = 'ranel';
     //string. IP or FQDN to the database
-    const HOST = "127.0.0.1";
+    const HOST = '127.0.0.1';
     //string. Username to the database
-    const USERNAME = "root";
+    const USERNAME = 'root';
     //string. Password to the database
-    const PASSWORD = "secret";
+    const PASSWORD = 'secret';
 }
-
-?>
